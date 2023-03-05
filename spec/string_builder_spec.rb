1e1915511819909cecafac9e99787b0bd1e93ef2
Farzan <58693804+Farzan-I@users.noreply.github.com>
2023-03-05 14:40:05 +0000

    Refactor spec file for string_builder

--- spec/string_builder_spec.rb
+++ spec/string_builder_spec.rb
@@ -1,27 +1,15 @@
 require 'string_builder'
 
 RSpec.describe StringBuilder do
-  it "initially returns an empty string as output" do
-    string_builder = StringBuilder.new
-    expect(string_builder.output).to eq ""
-  end
-
-  it "initially returns a size of 0" do
-    string_builder = StringBuilder.new
-    expect(string_builder.size).to eq 0
-  end
-
-  context "given one addition of a string" do
-    it "outputs that string" do
+  context "initially " do
+    it "initially returns an empty string as output" do
       string_builder = StringBuilder.new
-      string_builder.add("Hello")
-      expect(string_builder.output).to eq "Hello"
+      expect(string_builder.output).to eq ""
     end
 
-    it "has a size of that string's length" do
+    it "initially returns a size of 0" do
       string_builder = StringBuilder.new
-      string_builder.add("Hello")
-      expect(string_builder.size).to eq 5
+      expect(string_builder.size).to eq 0
     end
   end
 
