e6a5e140dfb5ade20d27055baecb7f52cbcc70de
Farzan <58693804+Farzan-I@users.noreply.github.com>
2023-03-05 14:16:13 +0000

    Create more tests to determine if class can count multiple given numbers

--- spec/counter_spec.rb
+++ spec/counter_spec.rb
@@ -1,10 +1,22 @@
 require 'counter'
 
 RSpec.describe Counter do
+  it "initially reports a count of zero" do
+    counter = Counter.new
+    expect(counter.report).to eq "Counted to 0 so far."
+  end
+
+  it "reports a count of all added values" do
+    counter = Counter.new
+    counter.add(5)
+    counter.add(4)
+    counter.add(6)
+    expect(counter.report).to eq "Counted to 15 so far."
+  end
+
   it "counts to a given number" do
     counter = Counter.new
     counter.add(6)
-    result = counter.report
-    expect(result).to eq "Counted to 6 so far."
+    expect(counter.report).to eq "Counted to 6 so far."
   end
 end
