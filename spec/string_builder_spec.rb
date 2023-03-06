c51d2f1a8dbb092b5efd1c113075e18ca4f94046
Farzan <58693804+Farzan-I@users.noreply.github.com>
2023-03-06 09:09:26 +0000

    Create spec file for gratitudes.rb with all tests passing

--- spec/string_builder_spec.rb
+++ spec/string_builder_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe StringBuilder do
     end
   end
 
-  context "given many addition of a string" do
+  context "given many additions of a string" do
     it "outputs that string" do
       string_builder = StringBuilder.new
       string_builder.add("Hello")
@@ -30,12 +30,12 @@ RSpec.describe StringBuilder do
       expect(string_builder.size).to eq 13
     end
   end
-
-# Not good to have two expectations as you're testing to different behaviours
-  # it "creates a string for a user" do
-  #   string_builder = StringBuilder.new
-  #   string_builder.add("Hello")
-  #   expect(string_builder.size).to eq 5
-  #   expect(string_builder.output).to eq "Hello"
-  # end
 end
+
+# Not good to have two expectations as you're testing two different behaviours
+# it "creates a string for a user" do
+#   string_builder = StringBuilder.new
+#   string_builder.add("Hello")
+#   expect(string_builder.size).to eq 5
+#   expect(string_builder.output).to eq "Hello"
+# end
