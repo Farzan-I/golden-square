7c2105efe285af1fb6a0e26bb0aae800c7b5e137
Farzan <58693804+Farzan-I@users.noreply.github.com>
2023-03-07 09:45:50 +0000

    Delete duplicate spec files

--- spec/greet_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'greet'
-
-RSpec.describe "greet method" do
-  it 'returns Hello with the input name' do
-    name_given = greet("Jonny")
-    expect(name_given).to eq "Hello, Jonny!"
-  end
-end
--- spec/report_length_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'report_length'
-
-RSpec.describe 'report_length method' do
-  it 'checks if the correct length of a given string matches the output' do
-    result = report_length("length")
-    expect(result).to eq "This string is 6 characters long"
-  end
-end
