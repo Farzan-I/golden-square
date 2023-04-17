e50701e1eade3fbfeaf435e38a0b481a943e0dfa
Farzan <58693804+Farzan-I@users.noreply.github.com>
2023-04-17 20:12:25 +0100

    Remove commented out previous check methods

--- phase-two/lib/grammar_stats.rb
+++ phase-two/lib/grammar_stats.rb
@@ -3,28 +3,6 @@ class GrammarStats
     # ...
   end
 
-  # def check(text)
-  #   fail "Text can't be given empty" if text.empty?
-
-  #   first_letter_capital = text[0] == text[0].upcase
-  #   last_character_is_correct = [".", "!", "?"].include? text[-1]
-
-  #   if first_letter_capital && last_character_is_correct
-  #     return true
-  #   else
-  #     false
-  #   end
-  # end
-
-  # def check(text)
-  #   raise "Text can't be given empty" if text.empty?
-    
-  #   first_letter_capital = text[0].upcase == text[0]
-  #   last_character_is_correct = [".", "!", "?"].include?(text[-1])
-    
-  #   first_letter_capital && last_character_is_correct
-  # end
-
   def check(text)
     fail "Text can't be given empty" if text.empty?
   
